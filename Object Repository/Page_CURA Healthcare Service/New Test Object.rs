3d3758eb84f1b3b93c9d0b80db635f81f40a7825
brice <brice@hotmail.com>
2018-09-20 11:06:10 +0200

    2nd commit

---
+++ Object Repository/Page_CURA Healthcare Service/New Test Object.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>3e2bc0fe-f981-43e0-8e97-8bfe7bcf0df7</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
